aeeda18d402e9905e7f6c5de398067df01ddcb73
[Mateus] <143464157+Mateus-lang@users.noreply.github.com>
2025-02-13 23:16:22 -0300

    feat: ensure booking details can be viewed

---
+++ app/_components/booking-summary.tsx
@@ -0,0 +1,6 @@
+//TODO: CRIAR COMPONENTE DE RESUMO DE AGENDAMENTO
+const BookingSummary = () => {
+  return <></>
+}
+
+export default BookingSummary
--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -46,7 +46,7 @@ const bookings = async () => {
     <>
       <Header />
       <div className="space-y-3 p-5">
-        <h1 className="text-xl font-bold">Agendamentos</h1>
+        <h1 className="mb-5 text-xl font-bold">Agendamentos</h1>
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Confirmados
         </h2>
