44db464868eac795830d5ceffa291e7941d343ef
[Mateus] <143464157+Mateus-lang@users.noreply.github.com>
2025-02-16 22:22:12 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   //chamar meu banco de dados
@@ -44,8 +46,19 @@ const Home = async () => {
       {/* TEXTO */}
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Rodrigo</h2>
-        <p>Domingo, 26 de Janeiro</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "Bem Vindo!"}
+        </h2>
+
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd ", { locale: ptBR })}
+          </span>
+          de
+          <span className="capitalize">
+            {format(new Date(), " MMMM", { locale: ptBR })}
+          </span>
+        </p>
 
         {/* BUSCA */}
         <div className="mt-6">
