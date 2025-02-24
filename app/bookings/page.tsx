d19fc07787e1893b02d2f528445906535c451f21
[Mateus] <143464157+Mateus-lang@users.noreply.github.com>
2025-02-24 06:17:03 -0300

    feat: add BookingSummary component

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -47,6 +47,9 @@ const bookings = async () => {
       <Header />
       <div className="space-y-3 p-5">
         <h1 className="mb-5 text-xl font-bold">Agendamentos</h1>
+        {confirmedBookings.length === 0 && concludedBookings.length === 0 && (
+          <p className="text-gray-400">Você não tem agendamentos.</p>
+        )}
         {confirmedBookings.length > 0 && (
           <>
             <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
