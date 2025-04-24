062ff7713601c0311e45c3192e52232c59ca20b6
[Mateus] <143464157+Mateus-lang@users.noreply.github.com>
2025-04-24 12:44:08 -0300

    feat: add custom types to ThemeProvider, improving property typing

--- app/_components/theme-provider.tsx
+++ app/_components/theme-provider.tsx
@@ -1,7 +1,19 @@
 "use client"
 
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+
+// Defina o tipo Attribute para corresponder ao esperado pelo NextThemesProvider
+type Attribute = "class" | "data-theme" | "data-mode"
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+  attribute?: Attribute | Attribute[]
+  defaultTheme?: string
+  enableSystem?: boolean
+  storageKey?: string
+  forcedTheme?: string
+  disableTransitionOnChange?: boolean
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
