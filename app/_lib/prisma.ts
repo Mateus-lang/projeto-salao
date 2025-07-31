7a56795437e90b987eb7243b5dda050820b1dd3e
[Mateus] <143464157+Mateus-lang@users.noreply.github.com>
2025-07-31 12:40:39 -0300

    feat: configure Prisma Client logging for production and development environments

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -3,11 +3,16 @@ import { PrismaClient } from "@prisma/client"
 const globalForPrisma = globalThis as unknown as { cachedPrisma?: PrismaClient }
 
 let prisma: PrismaClient
+
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
+  prisma = new PrismaClient({
+    log: ["error"],
+  })
 } else {
   if (!globalForPrisma.cachedPrisma) {
-    globalForPrisma.cachedPrisma = new PrismaClient()
+    globalForPrisma.cachedPrisma = new PrismaClient({
+      log: ["query", "error", "warn"],
+    })
   }
   prisma = globalForPrisma.cachedPrisma
 }
